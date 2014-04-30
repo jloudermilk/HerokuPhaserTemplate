4e79501cc28f7222902a1a9779e00dd0e5535424
Justin Loudermilk <lord.kuragari@gmail.com>
2014-04-29 20:56:29 -0400

    stuff

---
+++ web.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var express = require("express");
+var fs = require("fs");
+
+var app = express();
+
+// configure path root
+app.configure(function() {
+	app.use(express.static(__dirname + "/public"));
+});
+
+// match get requests
+app.get("/", function(_req, _res) {
+	_res.render("./public/index.html");
+});
+
+var port = process.env.PORT || 7000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
